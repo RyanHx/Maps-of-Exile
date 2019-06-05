20cdf107a9301e513dd972d2a8ec9823204848ba
RyanHx <hollick5@hotmail.com>
2019-06-05 23:22:46 +0100

    Placeholder update to test auto-updater

--- PoEMapsViewModel/Helper/Updater.cs
+++ PoEMapsViewModel/Helper/Updater.cs
@@ -3,7 +3,6 @@ using Squirrel;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
